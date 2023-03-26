b48195349ecf93487c11487d6608638988e90cbe
gilagajet <84836081+gilagajet@users.noreply.github.com>
2023-03-26 14:44:54 +0800

    Update diy-part2.sh

--- devices/GL.iNet-MT300N-V2/diy-part2.sh
+++ devices/GL.iNet-MT300N-V2/diy-part2.sh
@@ -35,6 +35,6 @@ sed -i 's/3.openwrt.pool.ntp.org/my.pool.ntp.org/g' package/base-files/files/bin
 
 
 ###Script
-wget https://raw.githubusercontent.com/gilagajet/gen/xray-1.5.4/import_feeds.sh
+wget https://raw.githubusercontent.com/gilagajet/gen/xray-latest/import_feeds.sh
 chmod +x import_feeds.sh
 ./import_feeds.sh
