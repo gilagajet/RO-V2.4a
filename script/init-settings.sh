3fcb6320d2f050558a84f4014a8bf22b1c10e068
gilagajet <84836081+gilagajet@users.noreply.github.com>
2023-06-28 11:39:39 +0800

    Update init-settings.sh

--- script/init-settings.sh
+++ script/init-settings.sh
@@ -319,28 +319,28 @@ uci commit system
 #-----------------------------------------------------------------------------
 
 # Disable IPv6
-uci set 'network.lan.ipv6=0'
-uci set 'network.wan.ipv6=0'
-uci set 'dhcp.lan.dhcpv6=disabled'
+#uci set 'network.lan.ipv6=0'
+#uci set 'network.wan.ipv6=0'
+#uci set 'dhcp.lan.dhcpv6=disabled'
 
-/etc/init.d/odhcpd stop
-/etc/init.d/odhcpd disable
+#/etc/init.d/odhcpd stop
+#/etc/init.d/odhcpd disable
 
-uci set network.lan.delegate="0"
+#uci set network.lan.delegate="0"
 
-uci -q delete network.globals.ula_prefix
+#uci -q delete network.globals.ula_prefix
 
-uci -q delete dhcp.lan.dhcpv6
-uci -q delete dhcp.lan.ra
+#uci -q delete dhcp.lan.dhcpv6
+#uci -q delete dhcp.lan.ra
 
-uci -q delete dhcp.lan.ra_slaac
-uci -q delete dhcp.lan.ndp
-uci -q delete dhcp.lan.ra_flags
+#uci -q delete dhcp.lan.ra_slaac
+#uci -q delete dhcp.lan.ndp
+#uci -q delete dhcp.lan.ra_flags
 
-uci delete network.wan6
+#uci delete network.wan6
 
-uci commit
-/etc/init.d/network restart
+#uci commit
+#/etc/init.d/network restart
 
 #-----------------------------------------------------------------------------
 
