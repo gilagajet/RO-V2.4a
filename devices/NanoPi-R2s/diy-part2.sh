6f236355a4c381ceaa3a59cb4cf30cdace13fed9
gilagajet <84836081+gilagajet@users.noreply.github.com>
2023-10-08 23:42:58 +0800

    Update diy-part2.sh

--- devices/NanoPi-R2s/diy-part2.sh
+++ devices/NanoPi-R2s/diy-part2.sh
@@ -18,7 +18,7 @@ sed -i 's/wireless.radio${devidx}.disabled=1/wireless.radio${devidx}.disabled=0/
 
 # Version Update
 sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='GilaGajet build $(TZ=UTC+8 date "+%Y.%m") '" >> package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='GilaGajet build $(TZ=UTC+8 date "+%Y.%m") Lite'" >> package/base-files/files/etc/openwrt_release
 sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
 echo "DISTRIB_REVISION='[OpenWRT v22.03.5]'" >> package/base-files/files/etc/openwrt_release
 
@@ -91,36 +91,71 @@ rm -rf ./feeds/packages/net/pdnsd-alt
 rm -rf ./feeds/packages/net/redsocks2
 
 # Dependencies
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/brook feeds/packages/net/brook
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng feeds/packages/net/chinadns-ng
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/dns2tcp feeds/packages/net/dns2tcp
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria feeds/packages/net/hysteria
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks feeds/packages/net/ssocks
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go feeds/packages/net/trojan-go
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus feeds/packages/net/trojan-plus
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/sagernet-core feeds/packages/net/sagernet-core
+#rm -rf ./feeds/packages/net/brook
+#rm -rf ./feeds/packages/net/chinadns-ng
+#rm -rf ./feeds/packages/net/dns2socks
+#rm -rf ./feeds/packages/net/ipt2socks
+#rm -rf ./feeds/packages/net/kcptun
+#rm -rf ./feeds/packages/net/microsocks
+#rm -rf ./feeds/packages/net/naiveproxy
+rm -rf ./feeds/packages/net/openssl1.1
+#rm -rf ./feeds/packages/net/pdnsd-alt
+#rm -rf ./feeds/packages/net/shadowsocksr-libev
+#rm -rf ./feeds/packages/net/simple-obfs
+#rm -rf ./feeds/packages/net/ssocks
+#rm -rf ./feeds/packages/net/tcping
+#rm -rf ./feeds/packages/net/trojan-go
+#rm -rf ./feeds/packages/net/trojan-plus
+#rm -rf ./feeds/packages/net/trojan
+rm -rf ./feeds/packages/net/v2ray-plugin
+rm -rf ./feeds/packages/net/v2ray
+rm -rf ./feeds/packages/net/xray
 
-svn export https://github.com/fw876/helloworld/trunk/naiveproxy feeds/packages/net/naiveproxy
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/brook feeds/packages/net/brook
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/chinadns-ng feeds/packages/net/chinadns-ng
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/dns2socks feeds/packages/net/dns2socks
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/ipt2socks feeds/packages/net/ipt2socks
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/kcptun feeds/packages/net/kcptun
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/microsocks feeds/packages/net/microsocks
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/naiveproxy feeds/packages/net/naiveproxy
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/openssl1.1 feeds/packages/net/openssl1.1
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/pdnsd-alt feeds/packages/net/pdnsd-alt 
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/shadowsocksr-libev feeds/packages/net/shadowsocksr-libev
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/simple-obfs feeds/packages/net/simple-obfs
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/ssocks feeds/packages/net/ssocks
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/tcping feeds/packages/net/tcping
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/trojan-go feeds/packages/net/trojan-go
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/trojan-plus feeds/packages/net/trojan-plus
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/trojan feeds/packages/net/trojan
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/v2ray feeds/packages/net/v2ray
+#svn export https://github.com/miaozilong/openwrt-passwall/trunk/v2ray-plugin feeds/packages/net/v2ray-plugin
+svn export https://github.com/miaozilong/openwrt-passwall/trunk/xray feeds/packages/net/xray
+
+
+#svn export https://github.com/fw876/helloworld/trunk/naiveproxy feeds/packages/net/naiveproxy
 svn export https://github.com/immortalwrt/packages/trunk/net/shadowsocks-libev feeds/packages/net/shadowsocks-libev
 svn export https://github.com/fw876/helloworld/trunk/shadowsocks-rust feeds/packages/net/shadowsocks-rust
-svn export https://github.com/fw876/helloworld/trunk/shadowsocksr-libev feeds/packages/net/shadowsocksr-libev
-svn export https://github.com/fw876/helloworld/trunk/simple-obfs feeds/packages/net/simple-obfs
-svn export https://github.com/fw876/helloworld/trunk/tcping feeds/packages/net/tcping
-svn export https://github.com/fw876/helloworld/trunk/trojan feeds/packages/net/trojan
+#svn export https://github.com/fw876/helloworld/trunk/shadowsocksr-libev feeds/packages/net/shadowsocksr-libev
+#svn export https://github.com/fw876/helloworld/trunk/simple-obfs feeds/packages/net/simple-obfs
+#svn export https://github.com/fw876/helloworld/trunk/tcping feeds/packages/net/tcping
+#svn export https://github.com/fw876/helloworld/trunk/trojan feeds/packages/net/trojan
 svn export https://github.com/fw876/helloworld/trunk/v2ray-core feeds/packages/net/v2ray-core
 svn export https://github.com/fw876/helloworld/trunk/v2ray-geodata feeds/packages/net/v2ray-geodata
 svn export https://github.com/fw876/helloworld/trunk/v2ray-plugin feeds/packages/net/v2ray-plugin
 svn export https://github.com/fw876/helloworld/trunk/v2raya feeds/packages/net/v2raya
+
+
+
 svn export https://github.com/arqam999/openwrt-passwall/branches/xtls-175-mp/xray-core feeds/packages/net/xray-core
 svn export https://github.com/fw876/helloworld/trunk/xray-plugin feeds/packages/net/xray-plugin
 svn export https://github.com/fw876/helloworld/trunk/lua-neturl feeds/packages/net/lua-neturl
-svn export https://github.com/immortalwrt/packages/trunk/net/dns2socks feeds/packages/net/dns2socks
-svn export https://github.com/immortalwrt/packages/trunk/net/microsocks feeds/packages/net/microsocks
-svn export https://github.com/fw876/helloworld/trunk/ipt2socks feeds/packages/net/ipt2socks
-svn export https://github.com/immortalwrt/packages/trunk/net/pdnsd-alt feeds/packages/net/pdnsd-alt
+#svn export https://github.com/immortalwrt/packages/trunk/net/dns2socks feeds/packages/net/dns2socks
+#svn export https://github.com/immortalwrt/packages/trunk/net/microsocks feeds/packages/net/microsocks
+#svn export https://github.com/fw876/helloworld/trunk/ipt2socks feeds/packages/net/ipt2socks
+#svn export https://github.com/immortalwrt/packages/trunk/net/pdnsd-alt feeds/packages/net/pdnsd-alt
 svn export https://github.com/immortalwrt/packages/trunk/net/redsocks2 feeds/packages/net/redsocks2
 svn export https://github.com/immortalwrt/packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
-svn export https://github.com/immortalwrt/packages/trunk/net/kcptun feeds/packages/net/kcptun
+#svn export https://github.com/immortalwrt/packages/trunk/net/kcptun feeds/packages/net/kcptun
 git clone -b master --depth 1 https://github.com/jerrykuku/lua-maxminddb.git feeds/packages/net/lua-maxminddb
 svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/shortcut-fe package/kernel/shortcut-fe
 svn export https://github.com/immortalwrt/packages/trunk/net/dnsforwarder feeds/packages/net/dnsforwarder
@@ -158,6 +193,10 @@ svn export https://github.com/immortalwrt/luci/branches/master/applications/luci
 
 # IPv6 Helper
 svn export https://github.com/immortalwrt/immortalwrt/trunk/package/emortal/ipv6-helper package/addon/ipv6-helper
+
+# Fix Golang
+#rm -rf ./feeds/packages/lang/golang
+#svn export https://github.com/openwrt/packages/branches/master/lang/golang feeds/packages/lang
 
 # Change "Allow connection to 65535"
 sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
