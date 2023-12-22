115529f9ce10c649556aaab16d6e1adc9defa47e
gilagajet <84836081+gilagajet@users.noreply.github.com>
2023-12-22 21:43:13 +0800

    Update diy-part2.sh

--- devices/NanoPi-R2s/diy-part2.sh
+++ devices/NanoPi-R2s/diy-part2.sh
@@ -18,9 +18,9 @@ sed -i 's/wireless.radio${devidx}.disabled=1/wireless.radio${devidx}.disabled=0/
 
 # Version Update
 sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='GilaGajet build $(TZ=UTC+8 date "+%Y.%m") Lite'" >> package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='GilaGajet build $(TZ=UTC+8 date "+%Y.%m") '" >> package/base-files/files/etc/openwrt_release
 sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_REVISION='[OpenWRT v22.03.5]'" >> package/base-files/files/etc/openwrt_release
+echo "DISTRIB_REVISION='[OpenWRT v22.03.6]'" >> package/base-files/files/etc/openwrt_release
 
 # Update TimeZone
 sed -i 's/0.openwrt.pool.ntp.org/time.google.com/g' package/base-files/files/bin/config_generate
@@ -45,85 +45,92 @@ sed -i 's/3.openwrt.pool.ntp.org/my.pool.ntp.org/g' package/base-files/files/bin
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-git clone -b master --depth 1 https://github.com/immortalwrt/immortalwrt.git immortalwrt
-git clone -b openwrt-21.02 --depth 1 https://github.com/immortalwrt/immortalwrt.git immortalwrt_21
-git clone -b openwrt-23.05 --depth 1 https://github.com/immortalwrt/immortalwrt.git immortalwrt_23
-git clone -b master --depth 1 https://github.com/immortalwrt/packages.git immortalwrt_pkg
-git clone -b openwrt-21.02 --depth 1 https://github.com/immortalwrt/packages.git immortalwrt_pkg_21
-git clone -b master --depth 1 https://github.com/immortalwrt/luci.git immortalwrt_luci
-git clone -b openwrt-21.02 --depth 1 https://github.com/immortalwrt/luci.git immortalwrt_luci_21
-git clone -b openwrt-23.05 --depth 1 https://github.com/immortalwrt/luci.git immortalwrt_luci_23
-git clone -b master --depth 1 https://github.com/coolsnowwolf/lede.git lede
-git clone -b master --depth 1 https://github.com/coolsnowwolf/luci.git lede_luci
-git clone -b master --depth 1 https://github.com/coolsnowwolf/packages.git lede_pkg
-git clone -b main --depth 1 https://github.com/openwrt/openwrt.git openwrt_ma
-git clone -b openwrt-22.03 --depth 1 https://github.com/openwrt/openwrt.git openwrt_22
-git clone -b master --depth 1 https://github.com/openwrt/packages.git openwrt_pkg_ma
-git clone -b master --depth 1 https://github.com/openwrt/luci.git openwrt_luci_ma
-git clone -b master --depth 1 https://github.com/Lienol/openwrt.git Lienol
-git clone -b main --depth 1 https://github.com/Lienol/openwrt-package Lienol_pkg
-git clone -b packages --depth 1 https://github.com/xiaorouji/openwrt-passwall passwall_pkg
-git clone -b luci --depth 1 https://github.com/xiaorouji/openwrt-passwall passwall_luci
-git clone -b master --depth 1 https://github.com/fw876/helloworld ssrp
+# luci-theme-argon
+git clone -b master --depth 1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+git clone -b master --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
 
-git clone -b master --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/new/luci-theme-argon
-rm -rf ./package/new/luci-theme-argon/htdocs/luci-static/argon/background/README.md
-git clone -b master --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git package/new/luci-app-argon-config
-
-cp -rf ../OpenWrt-Add/luci-app-cpufreq ./feeds/luci/applications/luci-app-cpufreq
-ln -sf ../../../feeds/luci/applications/luci-app-cpufreq ./package/feeds/luci/luci-app-cpufreq
-sed -i 's,1608,1800,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
-sed -i 's,2016,2208,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
-sed -i 's,1512,1608,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
-cp -rf ../OpenWrt-Add/luci-app-cpulimit ./package/new/luci-app-cpulimit
-cp -rf ../immortalwrt_pkg/utils/cpulimit ./feeds/packages/utils/cpulimit
-ln -sf ../../../feeds/packages/utils/cpulimit ./package/feeds/packages/cpulimit
-
+# luci-theme-edge
 git clone -b master --depth 1 https://github.com/kiddin9/luci-theme-edge.git package/new/luci-theme-edge
 
-git clone -b master --depth 1 https://github.com/NateLol/luci-app-oled.git package/new/luci-app-oled
+# Autocore
+#svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/emortal/autocore feeds/packages/utils/autocore
+svn export https://github.com/breakings/OpenWrt/trunk/general/autocore feeds/packages/utils/autocore
+sed -i 's/"getTempInfo" /"getTempInfo", "getCPUBench", "getCPUUsage" /g' feeds/packages/utils/autocore/files/generic/luci-mod-status-autocore.json
 
-git clone --single-branch --depth 1 -b dev https://github.com/vernesong/OpenClash.git package/new/luci-app-openclash
+# Coremark
+rm -rf ./feeds/packages/utils/coremark
+svn export https://github.com/immortalwrt/packages/trunk/utils/coremark feeds/packages/utils/coremark
 
-cp -rf ../passwall_luci/luci-app-passwall ./package/new/luci-app-passwall
-cp -rf ../passwall_pkg/tcping ./package/new/tcping
-cp -rf ../passwall_pkg/trojan-go ./package/new/trojan-go
-cp -rf ../passwall_pkg/brook ./package/new/brook
-cp -rf ../passwall_pkg/ssocks ./package/new/ssocks
-cp -rf ../passwall_pkg/microsocks ./package/new/microsocks
-cp -rf ../passwall_pkg/dns2socks ./package/new/dns2socks
-cp -rf ../passwall_pkg/ipt2socks ./package/new/ipt2socks
-cp -rf ../passwall_pkg/pdnsd-alt ./package/new/pdnsd-alt
-cp -rf ../OpenWrt-Add/trojan-plus ./package/new/trojan-plus
-cp -rf ../passwall_pkg/xray-plugin ./package/new/xray-plugin
-
-cp -rf ../lede_luci/applications/luci-app-ramfree ./package/new/luci-app-ramfree
-
-rm -rf ./feeds/packages/net/shadowsocks-libev
-cp -rf ../lede_pkg/net/shadowsocks-libev ./package/new/shadowsocks-libev
-cp -rf ../ssrp/redsocks2 ./package/new/redsocks2
-cp -rf ../ssrp/trojan ./package/new/trojan
-cp -rf ../ssrp/tcping ./package/new/tcping
-cp -rf ../ssrp/dns2tcp ./package/new/dns2tcp
-cp -rf ../ssrp/gn ./package/new/gn
-cp -rf ../ssrp/shadowsocksr-libev ./package/new/shadowsocksr-libev
-cp -rf ../ssrp/simple-obfs ./package/new/simple-obfs
-cp -rf ../ssrp/naiveproxy ./package/new/naiveproxy
-cp -rf ../ssrp/v2ray-core ./package/new/v2ray-core
-cp -rf ../passwall_pkg/hysteria ./package/new/hysteria
-cp -rf ../ssrp/sagernet-core ./package/new/sagernet-core
-rm -rf ./feeds/packages/net/xray-core
-cp -rf ../ssrp/xray-core ./package/new/xray-core
-cp -rf ../ssrp/v2ray-plugin ./package/new/v2ray-plugin
-cp -rf ../ssrp/shadowsocks-rust ./package/new/shadowsocks-rust
-cp -rf ../ssrp/lua-neturl ./package/new/lua-neturl
+# Remove some net packages
+rm -rf ./feeds/packages/net/https-dns-proxy
 rm -rf ./feeds/packages/net/kcptun
-cp -rf ../immortalwrt_pkg/net/kcptun ./feeds/packages/net/kcptun
-ln -sf ../../../feeds/packages/net/kcptun ./package/feeds/packages/kcptun
+rm -rf ./feeds/packages/net/shadowsocks-libev
+rm -rf ./feeds/packages/net/xray-core
+rm -rf ./feeds/packages/net/trojan-go
+rm -rf ./feeds/packages/net/v2ray-core
+rm -rf ./feeds/packages/net/v2ray-geodata
 
-cp -rf ../ssrp/luci-app-ssr-plus ./package/new/luci-app-ssr-plus
+# Dependencies
+svn export https://github.com/quango-web/openwrt-passwall/trunk/brook feeds/packages/net/brook
+svn export https://github.com/quango-web/openwrt-passwall/trunk/chinadns-ng feeds/packages/net/chinadns-ng
+svn export https://github.com/quango-web/openwrt-passwall/trunk/dns2socks feeds/packages/net/dns2socks
+svn export https://github.com/quango-web/openwrt-passwall/trunk/dns2tcp feeds/packages/net/dns2tcp
+svn export https://github.com/quango-web/openwrt-passwall/trunk/hysteria feeds/packages/net/hysteria
 
-cp -rf ../immortalwrt_luci/applications/luci-app-zerotier ./feeds/luci/applications/luci-app-zerotier
+svn export https://github.com/fw876/helloworld/trunk/ipt2socks feeds/packages/net/ipt2socks
+
+svn export https://github.com/quango-web/openwrt-passwall/trunk/microsocks feeds/packages/net/microsocks
+svn export https://github.com/quango-web/openwrt-passwall/trunk/naiveproxy feeds/packages/net/naiveproxy
+svn export https://github.com/quango-web/openwrt-passwall/trunk/pdnsd-alt feeds/packages/net/pdnsd-alt
+svn export https://github.com/quango-web/openwrt-passwall/trunk/sagernet-core feeds/packages/net/sagernet-core
+svn export https://github.com/quango-web/openwrt-passwall/trunk/shadowsocks-rust feeds/packages/net/shadowsocks-rust
+svn export https://github.com/quango-web/openwrt-passwall/trunk/shadowsocksr-libev feeds/packages/net/shadowsocksr-libev
+svn export https://github.com/quango-web/openwrt-passwall/trunk/simple-obfs feeds/packages/net/simple-obfs
+svn export https://github.com/quango-web/openwrt-passwall/trunk/sing-box feeds/packages/net/sing-box
+svn export https://github.com/quango-web/openwrt-passwall/trunk/ssocks feeds/packages/net/ssocks
+svn export https://github.com/quango-web/openwrt-passwall/trunk/tcping feeds/packages/net/tcping
+svn export https://github.com/quango-web/openwrt-passwall/trunk/trojan-go feeds/packages/net/trojan-go
+svn export https://github.com/quango-web/openwrt-passwall/trunk/trojan-plus feeds/packages/net/trojan-plus
+svn export https://github.com/quango-web/openwrt-passwall/trunk/trojan feeds/packages/net/trojan
+svn export https://github.com/quango-web/openwrt-passwall/trunk/v2ray-core feeds/packages/net/v2ray-core
+svn export https://github.com/quango-web/openwrt-passwall/trunk/v2ray-geodata feeds/packages/net/v2ray-geodata
+svn export https://github.com/quango-web/openwrt-passwall/trunk/v2ray-plugin feeds/packages/net/v2ray-plugin
+
+svn export https://github.com/arqam999/openwrt-passwall/branches/xtls-175-mp/xray-core feeds/packages/net/xray-core
+
+svn export https://github.com/quango-web/openwrt-passwall/trunk/xray-plugin feeds/packages/net/xray-plugin
+
+svn export https://github.com/immortalwrt/packages/trunk/net/shadowsocks-libev feeds/packages/net/shadowsocks-libev
+
+svn export https://github.com/fw876/helloworld/trunk/lua-neturl feeds/packages/net/lua-neturl
+svn export https://github.com/immortalwrt/packages/trunk/net/redsocks2 feeds/packages/net/redsocks2
+svn export https://github.com/immortalwrt/packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
+svn export https://github.com/immortalwrt/packages/trunk/net/kcptun feeds/packages/net/kcptun
+
+git clone -b master --depth 1 https://github.com/jerrykuku/lua-maxminddb.git feeds/packages/net/lua-maxminddb
+svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/shortcut-fe package/kernel/shortcut-fe
+svn export https://github.com/immortalwrt/packages/trunk/net/dnsforwarder feeds/packages/net/dnsforwarder
+
+# luci-app-cpufreq
+svn export -r 19495 https://github.com/immortalwrt/luci/trunk/applications/luci-app-cpufreq feeds/luci/applications/luci-app-cpufreq
+
+# luci-app-openclash
+git clone --single-branch --depth 1 -b dev https://github.com/vernesong/OpenClash.git feeds/luci/applications/luci-app-openclash
+
+# luci-app-passwall
+svn export https://github.com/miaozilong/openwrt-passwall/branches/luci/luci-app-passwall feeds/luci/applications/luci-app-passwall
+
+# luci-app-ramfree
+svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-ramfree feeds/luci/applications/luci-app-ramfree
+
+# luci-app-zerotier
+svn export https://github.com/immortalwrt/luci/branches/master/applications/luci-app-zerotier feeds/luci/applications/luci-app-zerotier
+
+# IPv6 Helper
+svn export https://github.com/immortalwrt/immortalwrt/trunk/package/emortal/ipv6-helper package/addon/ipv6-helper
+
+# Change "Allow connection to 65535"
+sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
 # Update and Install Feeds
 ./scripts/feeds update -a
